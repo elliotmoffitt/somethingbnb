beb622e459436e0575607d98e532950f091e1bc6
Elliot <elliot.winton@gmail.com>
2025-03-16 14:44:47 -0400

    reviews half added

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect } from 'react';
 import './SpotDetails.css'
-import { fetchSpotDetails, loadSpots } from '../../store/spotReducer';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { fetchSpotDetails} from '../../store/spotReducer';
+import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchSpots } from '../../store/spotReducer';
-import Spot from '../Spot';
 
 const SpotDetails = () => {
     const dispatch = useDispatch();
@@ -16,19 +14,34 @@ const SpotDetails = () => {
     useEffect(() => {
         dispatch(fetchSpotDetails(params.spotId))
         setIsLoaded(true)
-    }, [dispatch]);
+    }, [dispatch, params.spotId]);
     // console.log(spot.previewImage, spot.name)
     console.log(spotDetails)
     return (
-        <div id="spot-details">
-            <h1>{spotDetails.name}</h1>
-            <h3>{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</h3>
-            {spotDetails.SpotImages ? spotDetails.SpotImages.map((image, i) => (
-                <div>
-                <img src={image.url}/>
+        <>
+            {isLoaded &&
+                <div id="spot-details">
+                    <div id="spot-details-details">
+                        <h1>{spotDetails.name}</h1>
+                        <h3>{spotDetails.city}, {spotDetails.state}, {spotDetails.country}</h3>
+                        {spotDetails.SpotImages ? spotDetails.SpotImages.map((image, i) => (
+                            <div key={`${i}-${image.id}`}>
+                                <img src={image.url} />
+                            </div>
+                        )) : ""}
+
+                        <div id="spot-details-name-description">
+                            <h3>Hosted by {`${spotDetails.Owner && spotDetails.Owner.firstName} ${spotDetails.Owner && spotDetails.Owner.lastName}`}</h3>
+                            <p>{spotDetails.description}</p>
+                        </div>
+                    </div>
+
+                    <div id="spot-details-reviews">
+                        asdf
+                    </div>
                 </div>
-            )) : ""}
-        </div>
+            }
+        </>
     );
 }
 
