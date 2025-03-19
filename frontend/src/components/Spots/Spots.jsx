df339878aa0c3361881e0efae3d88c3bb57bc6c5
Elliot <elliot.winton@gmail.com>
2025-03-18 23:52:50 -0400

    small changes

--- frontend/src/components/Spots/Spots.jsx
+++ frontend/src/components/Spots/Spots.jsx
@@ -1,7 +1,5 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './Spots.css'
-// import { loadSpots } from '../../store/spotReducer';
-// import { useSearchParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchSpots } from '../../store/spotReducer';
 import Spot from '../Spot';
@@ -9,44 +7,29 @@ import Spot from '../Spot';
 const Spots = () => {
     const dispatch = useDispatch();
     const spots = useSelector(state => state.spotState.entries)
-    // const [isLoaded, setIsLoaded] = useState(false);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         dispatch(fetchSpots())
-        // setIsLoaded(true)
+        setIsLoaded(true)
     }, [dispatch]);
-    // console.log(spots,)
-    // const [spots, setSpots] = useState([]);
 
-    // const getSpots = async () => {
-    //   try {
-    //     const res = await fetch('http://localhost:8000/api/spots');
-    //     const spotData = await res.json();
-    //     setSpots(spotData.Spots);
-    //   }
-    //   catch (e) {
-    //     console.log(e);
-    //     return [];
-    //   }
-    // }
-
-    // useEffect(() => {
-    //   getSpots();
-    // }, [])
-
-    return (
-        <div id='landing-page'>
-        <div id='spots'>
-        {spots.length ? spots.map((spot, i) => {
-            return (
-            <div key={`${i}-${spot.name}`}>
-                <Spot spot={spot}/>
+    if (isLoaded) {
+        return (
+            <div id='landing-page'>
+                <div id='spots'>
+                    {spots.length ? spots.map((spot, i) => {
+                        return (
+                            <div key={`${i}-${spot.name}`}>
+                                <Spot spot={spot} />
+                            </div>
+                        )
+                    }) : ""}
+                </div>
             </div>
-            )
-        }) : ""}
-        </div>
-        </div>
-    )
+        )
+    }
+    else return <h1>Loading...</h1>
 }
 
 export default Spots;
