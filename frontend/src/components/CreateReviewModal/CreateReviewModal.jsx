7677f3895f008057facde8302fd7b157c6f6873c
Elliot <elliot.winton@gmail.com>
2025-03-22 17:17:34 -0400

    review modal progress

--- frontend/src/components/CreateReviewModal/CreateReviewModal.jsx
+++ frontend/src/components/CreateReviewModal/CreateReviewModal.jsx
@@ -1,6 +1,13 @@
 import './CreateReviewModal.css';
+import {FaStar} from 'react-icons/fa';
 
 const CreateReviewModal = () => {
-    return <h1>asdfasdf</h1>
+    return (
+        <div id="review-modal">
+            <h1>How was your stay?</h1>
+            <textarea placeholder='Leave your review here...' id='review-box'></textarea>
+            <FaStar/>
+        </div>
+    )
 }
 export default CreateReviewModal;
--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -1,20 +1,52 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './Reviews.css'
 import { useSelector } from 'react-redux';
+import OpenModalMenuItem from '../Navigation/OpenModalMenuItem';
+import CreateReviewModal from '../CreateReviewModal/CreateReviewModal';
+
 
 const Reviews = () => {
     const reviews = useSelector(state => state.spotsStore.entries.reviews)
     const [isLoaded, setIsLoaded] = useState(false);
+    const [showMenu, setShowMenu] = useState(false);
+    const ulRef = useRef();
+
+    const toggleMenu = (e) => {
+        e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+        setShowMenu(!showMenu);
+    };
+
     useEffect(() => {
         setIsLoaded(true)
-    })
+        if (!showMenu) return;
+
+        const closeMenu = (e) => {
+            if (!ulRef.current.contains(e.target)) {
+                setShowMenu(false);
+            }
+        };
+
+        document.addEventListener('click', closeMenu);
+
+        return () => document.removeEventListener("click", closeMenu);
+    }, [showMenu]);
+
+    const closeMenu = () => setShowMenu(false);
+
+
     if (isLoaded) {
         return (
             <div>
                 {reviews && reviews.length ? reviews.map((review, i) => {
                     return (
                         <>
-                            <button id='review-button'>Post Your Review</button>
+                            <button id='review-button' onClick={toggleMenu}>
+                                <OpenModalMenuItem
+                                    itemText="Post Your Review"
+                                    onItemClick={closeMenu}
+                                    modalComponent={<CreateReviewModal />}
+                                />
+                                </button>
                             <div key={`${review.id}-${i}`} id='reviews'>
                                 <h3 className='review-details'>{review.User.firstName} {review.User.lastName}</h3>
                                 <h4 className='review-details'>Created: {Date(review.createdAt).split(' ').slice(0, 5).join(' ')}</h4>
